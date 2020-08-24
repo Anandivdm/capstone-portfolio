cfe86f51fe2eef044d91cf480d47aa52df2882c2
Anandi Ellis <anandivdm@gmail.com>
2020-08-24 10:33:46 +0200

    styling

---
+++ js/app.js
@@ -0,0 +1,22 @@
+let projects = [
+  {
+    name: 'Interactive Dashboard',
+    languages: 'HTML • CSS • JAVASCRIPT',
+    description: 'I built an interactive dashboard for a web application.'
+  },
+  {
+    name: 'Interactive Dashboard',
+    languages: 'HTML • CSS • JAVASCRIPT',
+    description: 'I built an interactive dashboard for a web application.'
+  },
+  {
+    name: 'Interactive Dashboard',
+    languages: 'HTML • CSS • JAVASCRIPT',
+    description: 'I built an interactive dashboard for a web application.'
+  },
+  {
+    name: 'Interactive Dashboard',
+    languages: 'HTML • CSS • JAVASCRIPT',
+    description: 'I built an interactive dashboard for a web application.'
+  }
+]
